1050b908211098fd18fe48059445f7e30ff315dc
Angel Torres <angelisraeltorres@outlook.com>
2022-12-29 01:15:24 +0000

    Saved progress on Instantiation 2022-12-29 01:15:24 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String s = new String("Hello");
+        return s;
     }
 }
