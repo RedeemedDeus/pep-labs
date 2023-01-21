efa278d19647ce20f142412eb2855cd3c2fa32d0
Angel Torres <angelisraeltorres@outlook.com>
2023-01-21 21:13:10 +0000

    Saved progress on IsSorted 2023-01-21 21:13:10 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -7,6 +7,17 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        for(int x = 0; x < arr.length; x++)
+        {
+            for(int y = x + 1; y < arr.length; y++)
+            {
+                if(arr[x] > arr[y])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
