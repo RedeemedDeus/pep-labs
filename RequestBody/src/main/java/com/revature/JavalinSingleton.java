1accbafd58aa69168ce9f6370e1e5b46900099ec
Angel Torres <angelisraeltorres@outlook.com>
2023-01-21 20:50:45 +0000

    Saved progress on RequestBody 2023-01-21 20:50:45 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -18,6 +20,14 @@ public class JavalinSingleton {
          */
         app.post("/problem1", ctx -> {
                 //implement logic here
+                String jsonString = ctx.body();
+
+                ObjectMapper om = new ObjectMapper();
+                Song song = om.readValue(jsonString, Song.class);
+
+                ctx.contentType("application/json");
+
+                ctx.result(song.getArtistName());
         });
 
         /**
@@ -29,6 +39,18 @@ public class JavalinSingleton {
          */
         app.post("/problem2", ctx -> {
                //implement logic here
+               String jsonString = ctx.body();
+
+               ObjectMapper om = new ObjectMapper();
+               Song song = om.readValue(jsonString, Song.class);
+
+               ctx.contentType("application/json");
+
+               song.setArtistName("Beatles");
+
+               String jsonreturn = om.writeValueAsString(song);
+
+               ctx.result(jsonreturn);
         });
 
 
