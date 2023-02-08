e32922e188b3017243a48a4a0b491673b12c3ddb
Angel Torres <angelisraeltorres@outlook.com>
2023-02-08 19:17:34 +0000

    Saved progress on ParentObjectClassMethods 2023-02-08 19:17:34 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -56,9 +56,16 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other){
+        if((this.a == other.a) && (this.b == other.b)) return true;
+        return false;
+    }
 
 
     //    implement a custom .toString(){} method here.
+    public String toString(){
+        return Integer.toString(a) + Boolean.toString(b);
+    }
 
     
 }
