1585c691584947d5923f9c1c6b168da164c8d61a
Angel Torres <angelisraeltorres@outlook.com>
2023-02-18 05:54:07 +0000

    Saved progress on StringIsPalindrome 2023-02-18 05:54:07 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,9 +12,9 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        String revstr = new StringBuilder(str).reverse().toString();
-
-        if(revstr == str) return true;
-        return false;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) return false;
+        }
+        return true;
     }
 }
